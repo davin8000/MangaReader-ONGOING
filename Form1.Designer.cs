26b0e8fbfc70a1bfcdd1d461cfdc7a38b282bb2a
davin8000 <cs.davin@gmail.com>
2016-01-06 23:08:28 +0800

    Initial Commit
    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace MangaReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(159, 237);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Manga";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 270);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Manga Reader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using TreeViewSerialization;
+
+namespace MangaReader
+{
+    public partial class Form1 : Form
+    {
+        TreeViewSerializer treexml = new TreeViewSerializer();
+        string filename = "Manga.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(filename)) treexml.DeserializeTreeView(treeView1, filename);//load saved treeview
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)//add manga
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.ShowNewFolderButton = false;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ListDirectory(treeView1, folderBrowserDialog1.SelectedPath);
+                save_TreeView_State();
+            }
+        }
+
+        private void save_TreeView_State()
+        {
+            treexml.SerializeTreeView(treeView1, filename);
+            treeView1.Nodes.Clear();
+            treexml.DeserializeTreeView(treeView1, filename);
+        }
+
+        private static void ListDirectory(TreeView treeView, string path)
+        {
+            var stack = new Stack<TreeNode>();
+            var rootDirectory = new DirectoryInfo(path);
+            var node = new TreeNode(rootDirectory.Name) { Tag = rootDirectory };//root folder node
+            stack.Push(node);
+            while (stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+                var directoryInfo = (DirectoryInfo)currentNode.Tag;
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    var childDirectoryNode = new TreeNode(directory.Name) { Tag = directory };//sub folder node
+                    currentNode.Nodes.Add(childDirectoryNode);
+                    stack.Push(childDirectoryNode);
+                }
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    if(file.Extension!=".db")//filter for thumbs.db
+                        currentNode.Nodes.Add(new TreeNode(file.Name) { Tag = file });//file node
+                }
+            }
+            treeView.Nodes.Add(node);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            /*TreeNode selected = null;
+            try { selected = treeView1.SelectedNode; }
+            catch { }
+            try { selected = treeView1.SelectedNode.FirstNode; }
+            catch { }
+            try { selected = treeView1.SelectedNode.FirstNode.FirstNode; }
+            catch { }
+            treeView1.SelectedNode = selected;
+            if (treeView1.SelectedNode != selected) treeView1_AfterSelect(this, e);*/
+            string selected = null;
+            try { selected = Path.Combine(treeView1.SelectedNode.Parent.Parent.Tag.ToString(), treeView1.SelectedNode.Parent.Tag.ToString(), treeView1.SelectedNode.Tag.ToString()); }
+            catch { }
+            try { selected = Path.Combine(treeView1.SelectedNode.Parent.Tag.ToString(), treeView1.SelectedNode.Tag.ToString(), treeView1.SelectedNode.FirstNode.Tag.ToString()); }
+            catch { }
+            try { selected = Path.Combine(treeView1.SelectedNode.Tag.ToString(), treeView1.SelectedNode.FirstNode.Tag.ToString(), treeView1.SelectedNode.FirstNode.FirstNode.Tag.ToString()); }
+            catch { }
+            label1.Text = selected;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //label1.Text = selected;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            treexml.SerializeTreeView(treeView1, filename);
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            //save_TreeView_State();
+        }
+    }
+}
